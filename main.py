8cf21cabfafb211537a939ffdb8148f5c4eeb266
Sadiqul Islam Shakib <sadiqul.islam.shakib21@gmail.com>
2026-04-04 13:07:00 +0600

    fix: make server port configurable via environment variable

--- main.py
+++ main.py
@@ -135,4 +135,5 @@ async def remove_bg(
 if __name__ == "__main__":
     import uvicorn
 
-    uvicorn.run(app, host="0.0.0.0", port=8000)
+    port = int(os.environ.get("PORT", 8000))
+    uvicorn.run(app, host="0.0.0.0", port=port)
